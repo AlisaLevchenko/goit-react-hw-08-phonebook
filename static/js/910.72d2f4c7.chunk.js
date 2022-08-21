f89a6e1cd356cd7d3a1dc2cd4b27366d5b2e5e84
AlisaLevchenko <103849753+AlisaLevchenko@users.noreply.github.com>
2022-08-21 13:36:51 +0000

    Deploying to gh-pages from @ AlisaLevchenko/goit-react-hw-08-phonebook@1849e792c6024b93ad2783d0e20c28b66db7752b 🚀

---
+++ static/js/910.72d2f4c7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[910],{5910:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"My Phonebook"})})}}}]);
+//# sourceMappingURL=910.72d2f4c7.chunk.js.map
